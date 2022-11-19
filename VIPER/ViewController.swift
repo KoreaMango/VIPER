6cbee0151ff8a4d302b92e3cad66b919ba69ad78
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-11-19 19:53:19 +0900

    Initial Commit

---
+++ VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER
+//
+//  Created by 강민규 on 2022/11/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
